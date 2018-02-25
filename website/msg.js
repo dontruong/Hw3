95a8d21e9ab93e513933786886f193bdf2bab7e3
dontruong <Truongdon1@yahoo.com>
2018-02-24 20:18:19 -0800

    done

--- website/msg.js
+++ website/msg.js
@@ -1,6 +1,34 @@
-const inputStr = prompt('Please input a count: ')
-const inputCount = parseInt(inputStr, 10)
-document.write("Starting Loop" + "<br />")
-for(let count = 0; count < inputCount; count++) { 
-    document.write("Current Count : " + count ) 
-    document.write("<br />"); } document.write("Loop stopped!")
+var x,y,z,sum;
+
+x = prompt('Please input the start: ')
+
+y  = prompt('Please input end: ')
+
+z = prompt('Please input step: ')
+
+
+document.write("array is " ,x+" ",y+" " , z+" " +"<br>" ); 
+
+x = parseInt(x);
+y = parseInt(y);
+z = parseInt(z);
+
+
+function sum (x,z,y){
+    return x+z+y
+}
+
+document.write("sum is "+sum(x,y,z)+"<br>");
+
+document.write((x.toString(2)).substr(-8)+",");
+
+document.write((y.toString(2)).substr(-8)+",");
+
+document.write((z.toString(2)).substr(-8)+",");
+
+if(isNaN(sum(x,y,z))){
+
+    alert ("one of inputs is not a number, refresh and try again ");
+    
+    }
+
